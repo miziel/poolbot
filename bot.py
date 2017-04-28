285b43f80512f7099e62b4da57f26de20f226ae8
endor <endor.xmr@gmail.com>
2017-04-28 00:36:08 +0000

    Added /poolluck command

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import ch
-import urllib, json
+import urllib, json, requests
 
 ##### TRIALS & TRIBULATIONS :)
 #url1 = "https://supportxmr.com/api/pool/stats/"
@@ -53,16 +53,28 @@ class bot(ch.RoomManager):
       prfx = False
       
     if cmd.lower() == "luck" and prfx:
-       url1 = "https://supportxmr.com/api/pool/stats/"
-       url2 = "https://supportxmr.com/api/network/stats/"
-       jsonDataPool = urllib.urlopen(url1)
-       jsonDataNetwork = urllib.urlopen(url2)
-       jsonToPython1 = json.loads(jsonDataPool.read())
-       jsonToPython2 = json.loads(jsonDataNetwork.read())
-       rShares = jsonToPython1['pool_statistics']['roundHashes']*100
-	   diff = jsonToPython2['difficulty']
-       luck = int(rShares/diff)
-       room.message("Current luck is %s%%" % str(luck))
+        url1 = "https://supportxmr.com/api/pool/stats/"
+        url2 = "https://supportxmr.com/api/network/stats/"
+        jsonDataPool = urllib.urlopen(url1)
+        jsonDataNetwork = urllib.urlopen(url2)
+        jsonToPython1 = json.loads(jsonDataPool.read())
+        jsonToPython2 = json.loads(jsonDataNetwork.read())
+        rShares = jsonToPython1['pool_statistics']['roundHashes']*100
+        diff = jsonToPython2['difficulty']
+        luck = int(rShares/diff)
+        room.message("Current block's luck is %s%%" % str(luck))
+    if cmd.lower() == "poolluck" and prfx:
+        poolstats = requests.get("https://supportxmr.com/api/pool/stats/").json()
+        blocknum = poolstats['pool_statistics']['totalBlocksFound']
+        blocklist = requests.get("https://supportxmr.com/api/pool/blocks/pplns?limit=" + str(blocknum)).json()
+        totaldiff = 0
+        totalshares = 0
+        startingblock = 1  # number of the starting block from which to scan the list
+        for i in reversed(range(0, blocknum-startingblock+1)):
+            totalshares += blocklist[i]['shares']
+            if blocklist[i]['valid'] == 1:
+                totaldiff += blocklist[i]['diff']
+        room.message("Overall pool luck is " + str(totalshares/totaldiff*100) + "%")
     
 rooms = [""]
 username = ""
