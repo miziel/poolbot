086771eb769a1854ecf265ecc92e1404e0376ff6
miziel <miziel@users.noreply.github.com>
2017-04-30 00:01:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,7 +62,26 @@ class bot(ch.RoomManager):
         rShares = jsonToPython1['pool_statistics']['roundHashes']*100
         diff = jsonToPython2['difficulty']
         luck = int(rShares/diff)
-        room.message("Current block's luck is %s%%" % str(luck))
+        if (luck > 0) and (luck <= 1):
+          room.message("Current block's luck is %s%% - seems like we just found one! *burger*" % str(luck))
+        elif (luck > 1) and (luck <= 20):
+          room.message("Current block's luck is %s%% - nice and low :)" % str(luck))
+        elif (luck > 20) and (luck <= 80):
+          room.message("Current block's luck is %s%% - looking good and green 8)" % str(luck))
+        elif (luck > 80) and (luck <= 100):
+          room.message("Current block's luck is %s%% - still green but..." % str(luck))
+        elif (luck > 100) and (luck <= 120):
+          room.message("Current block's luck is %s%% - a bit reddish." % str(luck))
+        elif (luck > 120) and (luck <= 150):
+          room.message("Current block's luck is %s%% - getting more red every hash :(" % str(luck))
+        elif (luck > 150) and (luck <= 200):
+          room.message("Current block's luck is %s%% - wouldn't mind finding one NOW!" % str(luck))
+        elif (luck > 200) and (luck <= 300):
+          room.message("Current block's luck is %s%% - damn time to find one, don't you think?" % str(luck))
+        elif (luck > 300) and (luck <= 400):
+          room.message("Current block's luck is %s%% - that's a lot of red." % str(luck))
+        else:
+          room.message("Current block's luck is %s%% - aiming for a new record, are we?" % str(luck))
     if cmd.lower() == "poolluck" and prfx:
         poolstats = requests.get("https://supportxmr.com/api/pool/stats/").json()
         blocknum = poolstats['pool_statistics']['totalBlocksFound']
@@ -75,9 +94,17 @@ class bot(ch.RoomManager):
             if blocklist[i]['valid'] == 1:
                 totaldiff += blocklist[i]['diff']
         room.message("Overall pool luck is " + str(totalshares*100/totaldiff) + "%")
-    
-rooms = [""]
-username = ""
+    if cmd.lower() == "price" and prfx: 
+        poloniex = requests.get("https://poloniex.com/public?command=returnTicker").json()
+        BTC_XMR_polo = poloniex['BTC_XMR']['last']
+        USDT_XMR_polo = poloniex['USDT_XMR']['last']
+        cryptocompare = requests.get("https://min-api.cryptocompare.com/data/price?fsym=XMR&tsyms=BTC,USD").json()
+        BTC_XMR_cc = cryptocompare['BTC']
+        USD_XMR_cc = cryptocompare['USD']
+        room.message("|| Poloniex | BTC {0} | USDT {1} || Cryptocompare | BTC {2} | USD {3} ||".format(BTC_XMR_polo[0:7], USDT_XMR_polo[0:5], BTC_XMR_cc, USD_XMR_cc))
+
+rooms = [""] #list rooms you want the bot to connect to
+username = "" #for tests can use your own - triger bot as anon
 password = ""
 
 bot.easy_start(rooms,username,password)
